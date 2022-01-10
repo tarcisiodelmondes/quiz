3d7adc00a642e559d793d17bfa0e5b938cd47b84
Tarcísio Delmondes <tarcisiodelmondes@gmail.com>
2022-01-10 18:24:43 -0300

    (on home_page) Refactor home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,27 @@
+import Image from 'next/image';
 import Link from 'next/link';
 import styles from '../styles/pages/home.module.scss';
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <main className={styles.container}>
+      <div className={styles.containerImg}>
+        <Image
+          src="/question-mark.svg"
+          alt="Image of a light bulb with a question mark"
+          width={300}
+          height={300}
+        />
+      </div>
+
+      <h1>
+        A quiz about sports in general.
+        <br /> <span>simple</span> and <span>fun</span>
+      </h1>
+
       <Link href="/page-questions">
         <a>Play Quiz</a>
       </Link>
-    </div>
+    </main>
   );
 }
